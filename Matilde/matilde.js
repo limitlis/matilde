c44cdaab5d4b12fc2b1d22be6cff640c42d27e49
Cesar Vargas <limitlis@gmail.com>
2018-06-05 13:15:43 -0700

    First commit of matilde lib

---
+++ Matilde/matilde.js
@@ -0,0 +1,79 @@
+function Matilde (inputValues) {
+    this._input = inputValues;
+    this.chunks = [];
+    this.sortChunks = [];
+    this.tree = {};
+    this.map = function (value, FOP) {
+        var me = this;
+        return Array.prototype.map.call(value, function (item) {
+            // Nesting
+            if (typeof item === 'object' && item.length) {
+
+                if (!FOP.FOP) {
+                    FOP.FOP = {};
+                }
+                me.chunks.push('FOP~0~(~0');
+                me.map(item, FOP.FOP);
+                me.chunks.push('FCP~0~)~0');
+            } else if (item) {
+
+                var filterType;
+
+                if (item.values && item.values.length) {
+                    filterType = 'FBL';
+                    // overwrite qualifier if using values instead of value (split on ,)
+                    item.qualifier = 'INN';
+                } else {
+                    filterType = 'FBV';
+                    // Handle wildcards for LK qualifier
+                    if (item.qualifier === 'LK') {
+                        item.value += encodeURIComponent('%');
+                        // if FBL/INN can use wildcards handle that here too
+                    }
+                }
+                if (item.op && item.op.toLowerCase() === 'or') {
+                    filterType += 'OR';
+                }
+
+                // handle sorts
+                if (item.sort) {
+                    if (item.sort !== 'ASC' && item.sort !== 'DESC') {
+                        // default sorting desc;
+                        item.sort = 'DESC';
+                    }
+                    var tempSortString = ['FSF', item.prop, item.sort, '0'].join('~');
+                    me.sortChunks.push(tempSortString);
+                    // sort goes at root of tree
+                    if (!me.tree.FSF) {
+                        me.tree.FSF = {};
+                    }
+                    me.tree.FSF[item.prop] = item.sort;
+                }
+                var filter = [filterType, item.prop, item.qualifier, (item.value || item.values.join(','))];
+                if (!FOP[filterType]) {
+                    FOP[filterType] = {};
+                }
+
+                FOP[filterType][item.prop] = [item.qualifier, (item.value || item.values.join(','))].join('~');
+
+                me.chunks.push(filter.join('~'));
+            }
+        });
+    };
+
+    if (this._input) {
+        this.map(this._input, this.tree);
+    }
+    return this;
+}
+
+Matilde.prototype.toString = function () {
+    if (this.chunks.length) {
+        // merge chunks with sortChunks
+        return this.chunks.concat(this.sortChunks).join('~');
+    } else if (!this._input){
+        throw new Error('no input');
+    }
+};
+
+module.exports = Matilde;
---
+++ test.js
@@ -0,0 +1,97 @@
+var chai = require('chai');
+var Matilde = require('./Matilde/Matilde');
+var assert = chai.assert;
+
+var testData1 = [
+    {
+        prop: 'ID',
+        qualifier: 'EQ',
+        value: '1'
+    },
+    [
+        {
+            prop: 'Role',
+            qualifier: 'LE',
+            value: '4',
+            op: 'OR'
+        }, {
+            prop: 'FirstName',
+            qualifier: 'LK',
+            value: 'Cesar',
+            op: 'OR',
+            sort: true
+        },
+        [
+            {
+                prop: 'LastName',
+                qualifier: 'INN',
+                values: ['Chavez', 'Vargas']
+            }
+        ]
+    ]
+];
+
+var data = new Matilde(testData1);
+
+describe('Matilde', function() {
+    it('should return a string representation of the input data', function() {
+        assert.equal(data.toString(), 'FBV~ID~EQ~1~FOP~0~(~0~FBVOR~Role~LE~4~FBVOR~FirstName~LK~Cesar%25~FOP~0~(~0~FBL~LastName~INN~Chavez,Vargas~FCP~0~)~0~FCP~0~)~0~FSF~FirstName~DESC~0');
+    });
+    it('should have 8 chunks', function() {
+        assert.equal(data.chunks.length, 8);
+    });
+    it('should properly use \'OR\' operator', function () {
+        assert.equal(data.tree.FOP.hasOwnProperty('FBVOR'), true);
+    });
+    it('should add wildcard (%) if qualifier is \'LK\'', function () {
+        assert.equal(data.tree.FOP.FBVOR.FirstName.indexOf('%25') > -1, true);
+    });
+    it('~should~have~an~insane~amount~of~tildes~!', function () {
+        var insaneAmount = 3;
+        var tildeLength = data.toString().split('~').length;
+        console.log('     has dis many', tildeLength);
+        assert.equal(tildeLength > insaneAmount, true);
+    });
+    describe('as a tree', function() {
+        it('should have proper nesting', function() {
+            var tempLevels = 0;
+            function getLevels(level) {
+                var tempKeys = Object.keys(level);
+                tempKeys.forEach(function(key) {
+                    if (key === 'FOP') {
+                        tempLevels++;
+                        getLevels(level[key]);
+                    }
+                });
+            }
+            getLevels(data.tree);
+
+            var levels = 0;
+            function countLevels(input) {
+                return input.forEach(function (level) {
+                    if (level.length) {
+                        levels++;
+                        countLevels(level);
+                    }
+                });
+            }
+            countLevels(data._input);
+            assert.equal(tempLevels, levels);
+        });
+        it ('should have sort filters', function () {
+            var sorts = [];
+            function hasSorting(input) {
+                return input.forEach(function (level) {
+                    if (level.length) {
+                        hasSorting(level);
+                    }
+                    if (level.hasOwnProperty('sort')) {
+                        sorts.push(level.prop);
+                    }
+                });
+            }
+            hasSorting(data._input);
+            assert.equal(Object.keys(data.tree.FSF).length, sorts.length);
+        });
+    });
+});
